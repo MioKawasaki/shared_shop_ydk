af51f771c5d31433e9f1a29a87ae218c8e9b81a4
k-ysd53 <k.yoshida.its.202504@gmail.com>
2025-06-10 16:46:35 +0900

    Update OrderItemRepository.java

--- src/main/java/jp/co/sss/shop/repository/OrderItemRepository.java
+++ src/main/java/jp/co/sss/shop/repository/OrderItemRepository.java
@@ -1,7 +1,12 @@
 package jp.co.sss.shop.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import jp.co.sss.shop.entity.Item;
 import jp.co.sss.shop.entity.OrderItem;
 
 /**
@@ -10,4 +15,13 @@ import jp.co.sss.shop.entity.OrderItem;
  * @author System Shared
  */
 public interface OrderItemRepository extends JpaRepository<OrderItem, Integer> {
+	/**
+	 * カテゴリIDと削除フラグを条件に売上順で取得
+	 * 
+	 * @param categoryId カテゴリID
+	 * @param deleteFlag 削除フラグ
+	 * @return 商品エンティティ
+	 */
+	@Query("SELECT o.item FROM OrderItem o WHERE (:categoryId IS NULL OR :categoryId = 0 OR o.item.category.id = :categoryId) AND o.item.deleteFlag = :deleteFlag GROUP BY o.item ORDER BY SUM(o.quantity) DESC")
+	List<Item> findByOrderItemSumQuantity(@Param("categoryId") Integer categoryId, @Param("deleteFlag") int deleteFlag);
 }
