ee38e21976cc393bfe65ed8d94c232a172b903a0
SashiTouto <tokyoitschool01360@gmail.com>
2025-06-11 11:28:14 +0900

    Add files via upload

---
+++ src/main/java/jp/co/sss/shop/controller/client/user/ClientUserDeleteController.java
@@ -0,0 +1,51 @@
+package jp.co.sss.shop.controller.client.user;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpSession;
+import jp.co.sss.shop.bean.UserBean;
+import jp.co.sss.shop.entity.User;
+import jp.co.sss.shop.form.UserForm;
+import jp.co.sss.shop.repository.UserRepository;
+
+@Controller
+public class ClientUserDeleteController {
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	@RequestMapping(path="/client/user/delete/check")
+	public String userDeleteCheck(Model model,HttpSession session) {
+		UserBean user = (UserBean)session.getAttribute("user");
+		UserForm userForm = new UserForm();
+//		userForm.setId(user.getId());
+//		userForm.setEmail(user.getEmail());
+//		userForm.setPassword(user.getPassword());
+//		userForm.setName(user.getName());
+//		userForm.setPostalCode(user.getPostalCode());
+//		userForm.setAddress(user.getAddress());
+//		userForm.setPhoneNumber(user.getPhoneNumber());
+//		userForm.setAuthority(user.getAuthority());
+		Optional<User> userOpt = userRepository.findById(user.getId());
+		User users = userOpt.get();
+		System.out.println(user.getId());
+		model.addAttribute("userForm",users);
+		//model.addAttribute("",);
+		return "client/user/delete_check";
+	}
+	
+	@RequestMapping(path="/client/user/delete/complete")
+	public String userDeleteComplete(HttpSession session) {
+		UserBean user = (UserBean)session.getAttribute("user");
+		Optional<User> userOpt = userRepository.findById(user.getId());
+		User users = userOpt.get();
+		users.setDeleteFlag(1);
+		userRepository.save(users);
+		return "client/user/delete_complete";
+	}
+}
---
+++ src/main/java/jp/co/sss/shop/controller/client/user/ClientUserRegistController.java
@@ -0,0 +1,70 @@
+package jp.co.sss.shop.controller.client.user;
+
+
+import java.sql.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import jp.co.sss.shop.entity.User;
+import jp.co.sss.shop.form.UserForm;
+import jp.co.sss.shop.repository.UserRepository;
+
+@Controller
+public class ClientUserRegistController {
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	@RequestMapping(path="/client/user/regist/input/init")
+	public String registUserInput(Model model) {
+		UserForm userForm = new UserForm();
+		model.addAttribute("userForm",userForm);
+		return "client/user/regist_input";
+	}
+	
+	@RequestMapping(path="/client/user/regist/check",method=RequestMethod.POST)
+	public String registUserCheck(Model model,@Valid UserForm userForm,BindingResult bindingResult,HttpSession session) {
+		session.setAttribute("userSession", userForm);
+		if(bindingResult.hasErrors()) {
+			model.addAttribute("userForm",userForm);
+			return "client/user/regist_input";
+		}else {
+			model.addAttribute("userForm",userForm);
+			return "client/user/regist_check";
+		}
+	}
+	
+	@RequestMapping(path="/client/user/regist/input",method=RequestMethod.POST)
+	public String registUserReInput(Model model,UserForm userForm,HttpSession session) {
+		model.addAttribute("userForm",(UserForm)session.getAttribute("userSession"));
+		return "client/user/regist_input";
+	}
+	
+	@RequestMapping(path="/client/user/regist/complete",method= {RequestMethod.GET,RequestMethod.POST})
+	public String registUserComplete(Model model,HttpSession session) {
+		System.out.println("テスト2");
+		UserForm userForm = (UserForm)session.getAttribute("userSession");
+		User user = new User();
+		user.setEmail(userForm.getEmail());
+		user.setPassword(userForm.getPassword());
+		user.setName(userForm.getName());
+		user.setPostalCode(userForm.getPostalCode());
+		user.setAddress(userForm.getAddress());
+		user.setPhoneNumber(userForm.getPhoneNumber());
+		user.setAuthority(2);
+		user.setDeleteFlag(0);
+		long sqlDateNow = System.currentTimeMillis();
+		Date now = new Date(sqlDateNow);
+		user.setInsertDate(now);
+		userRepository.save(user);
+		session.removeAttribute("userSession");
+		return "client/user/regist_complete";
+	}
+}
