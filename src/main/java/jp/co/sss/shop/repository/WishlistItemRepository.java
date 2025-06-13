2b82008f33e51b86bf799949643ddb12a696613a
k-ysd53 <k.yoshida.its.202504@gmail.com>
2025-06-13 11:04:30 +0900

    Update WishlistItemRepository.java
    コメント追加

--- src/main/java/jp/co/sss/shop/repository/WishlistItemRepository.java
+++ src/main/java/jp/co/sss/shop/repository/WishlistItemRepository.java
@@ -8,10 +8,18 @@ import org.springframework.data.repository.query.Param;
 
 import jp.co.sss.shop.entity.WishlistItem;
 
+/**
+ * wishlist_itemsテーブル用リポジトリ
+ * 
+ * @author YDK
+ */
 public interface WishlistItemRepository extends JpaRepository<WishlistItem, Integer> {
+	
+	// 商品IDと会員IDを条件にほしいもの情報を検索
 	@Query("SELECT w FROM WishlistItem w WHERE w.item.id = :itemId AND w.user.id = :userId")
 	WishlistItem findItemIdByWishlistItem(@Param("itemId") Integer itemId, @Param("userId") Integer userId);
 	
+	// 会員IDを条件に新着順でほしいもの情報を検索
 	@Query("SELECT w FROM WishlistItem w WHERE w.user.id = :id ORDER BY w.insertDate DESC")
 	List<WishlistItem> findUserIdByWishlistItemOrderByInsertDate(@Param("id") Integer id);
 }
